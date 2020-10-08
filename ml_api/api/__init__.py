89e2ac7280071da0651ee3a2bfab22abcd3f0b21
Emin Mammadov <emammadov@acerta.ca>
2020-10-08 11:50:34 +0400

    Folder restructure

--- ml_api/api/__init__.py
+++
@@ -1,3 +0,0 @@
-from ml_api.api.config import PACKAGE_ROOT
-
-
--- ml_api/run.py
+++
@@ -1,9 +0,0 @@
-from ml_api.api.app import create_app
-from ml_api.api.config import DevelopmentConfig
-
-application = create_app(config_object=DevelopmentConfig)
-
-
-if __name__ == '__main__':
-    application.run()
-
