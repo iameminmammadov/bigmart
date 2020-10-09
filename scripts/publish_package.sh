37a19f2e655dbe6b2d15889ff14e8fdb608bcf42
Emin Mammadov <emammadov@acerta.ca>
2020-10-09 15:10:20 +0400

    fixing the relative path

--- scripts/publish_package.sh
+++ scripts/publish_package.sh
@@ -1,2 +1,2 @@
-tar -zcvf datacube_bigmart.tar.gz datacube_bigmart
+tar -zcvf packages/datacube_bigmart/datacube_bigmart.tar.gz packages/datacube_bigmart/datacube_bigmart
 curl -F package=@packages/datacube_bigmart/datacube_bigmart.tar.gz $PIP_EXTRA_INDEX_URL
