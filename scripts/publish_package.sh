c009f1c79cde4caa9dbecf26860aec390ebf2d0e
Emin Mammadov <emammadov@acerta.ca>
2020-10-09 17:20:27 +0400

    corrected the bash script for publishing stage

--- scripts/publish_package.sh
+++ scripts/publish_package.sh
@@ -1,2 +1,14 @@
-tar -zcvf packages/datacube_bigmart/datacube_bigmart.tar.gz packages/datacube_bigmart/datacube_bigmart
-curl -F package=@packages/datacube_bigmart/datacube_bigmart.tar.gz $PIP_EXTRA_INDEX_URL
+BASE_DIR=$(pwd)
+cd '..'
+cd "$BASE_DIR/packages/datacube_bigmart"
+echo "Changing directory"
+pwd
+python setup.py sdist
+echo "Creating a tar package"
+cd '..'
+tar -cvzf datacube_bigmart.tar.gz datacube_bigmart
+echo "Publishing a package to GemFury"
+curl -F package=@datacube_bigmart.tar.gz $PIP_EXTRA_INDEX_URL
+
+
+#python setup.py sdist
