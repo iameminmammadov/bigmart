f8d696ee32eda2e44a64f3f1bae421fbd772b9c8
Emin Mammadov <emammadov@acerta.ca>
2020-10-10 22:09:07 +0400

    minor changes

--- packages/datacube_bigmart/datacube_bigmart/config.py
+++ packages/datacube_bigmart/datacube_bigmart/config.py
@@ -1,12 +1,13 @@
-from os.path import dirname, abspath, join
+from os.path import join
 import pandas as pd
+import os
 
 pd.options.display.max_rows = 100
 pd.options.display.max_columns = 100
 
-PACKAGE_ROOT = abspath(dirname(__file__))
-TRAINED_MODEL_DIR = join(PACKAGE_ROOT, 'trained_models')
-DATASET_DIR = join(PACKAGE_ROOT, 'datasets')
+PACKAGE_ROOT = os.getcwd()
+TRAINED_MODEL_DIR = join(PACKAGE_ROOT, 'datacube_bigmart/trained_models')
+DATASET_DIR = join(PACKAGE_ROOT, 'datacube_bigmart/datasets')
 
 TESTING_DATA_FILE = 'Test.csv'
 TRAINING_DATA_FILE = 'Train.csv'
