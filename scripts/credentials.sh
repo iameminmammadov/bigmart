39d572670aaf0aaba004e34ee58668dcfac58fc7
Emin Mammadov <emammadov@acerta.ca>
2020-10-11 14:16:26 +0400

    s3 config

--- scripts/credentials.sh
+++ scripts/credentials.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-aws configure --profile staging set $region
-aws configure --profile staging set $aws_access_key_id
-aws configure --profile staging set $aws_secret_access_key
+aws_access_key_id = $AWS_ACCESS_KEY_ID
+aws_secret_access_key = $AWS_SECRET_ACCESS_KEY
