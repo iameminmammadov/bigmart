f57014d474f851ec2e2f5528414d998bb5ed6901
Emin Mammadov <emammadov@acerta.ca>
2020-11-26 17:26:05 +0400

    moved to separate .sh

---
+++ scripts/publish_gemfury.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+BASE_DIR=$(pwd)
+cd '..'
+cd "$BASE_DIR/packages/datacube_bigmart"
+echo "Changing directory"
+pwd
+pip install --upgrade pip
+python setup.py install --force
+echo "Retraining step"
+python datacube_bigmart/train_pipeline.py
