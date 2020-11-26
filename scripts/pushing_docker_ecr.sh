21f526f6edff8569e1d72be8f156daa37e509dd3
Emin Mammadov <emammadov@acerta.ca>
2020-11-27 03:38:43 +0400

    fixed docker image name

--- scripts/pushing_docker_ecr.sh
+++ scripts/pushing_docker_ecr.sh
@@ -2,10 +2,11 @@
 aws_access_key_id = $AWS_ACCESS_KEY_ID
 aws_secret_access_key = $AWS_SECRET_ACCESS_KEY
 
+echo $AWS_DEFAULT_REGION
 
 $(aws ecr get-login --region $AWS_DEFAULT_REGION --no-include-email)
 echo "Tag Step"
-docker tag bigmart $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/bigmart_docker_ecr
+docker tag bigmart_lhl $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/bigmart_docker_ecr
 echo "Push Docker to ECR"
 docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/bigmart_docker_ecr
 
