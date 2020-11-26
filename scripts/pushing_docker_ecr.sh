9385fd288e888c1deae000d7764b289ada3642dc
Emin Mammadov <emammadov@acerta.ca>
2020-11-27 03:29:42 +0400

    ecr

--- scripts/pushing_docker_ecr.sh
+++ scripts/pushing_docker_ecr.sh
@@ -2,6 +2,7 @@
 aws_access_key_id = $AWS_ACCESS_KEY_ID
 aws_secret_access_key = $AWS_SECRET_ACCESS_KEY
 
+
 $(aws ecr get-login --region $AWS_DEFAULT_REGION --no-include-email)
 echo "Tag Step"
 docker tag bigmart $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/bigmart_docker_ecr
