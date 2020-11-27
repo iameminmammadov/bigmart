79bc67ac55f94dc5b200969c3f30cc0b06cdfd3d
Emin Mammadov <emammadov@acerta.ca>
2020-11-28 02:47:50 +0400

    adding .sh for GemFury

--- scripts/publish_gemfury.sh
+++ scripts/publish_gemfury.sh
@@ -3,8 +3,9 @@ BASE_DIR=$(pwd)
 cd '..'
 cd "$BASE_DIR/packages/datacube_bigmart"
 echo "Changing directory"
-pwd
-pip install --upgrade pip
 python setup.py install --force
-echo "Retraining step"
-python datacube_bigmart/train_pipeline.py
+echo "Creating a tar package"
+cd '..'
+tar -cvzf datacube_bigmart.tar.gz datacube_bigmart
+echo "Publishing a package to GemFury"
+curl -F package=@datacube_bigmart.tar.gz $PIP_EXTRA_INDEX_URL
